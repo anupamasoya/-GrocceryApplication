3b893a62698c130c17476bc6c97e1eecd13be275
Anupama S <Anupama S@LAPTOP-UCLTDI57>
2025-12-16 12:31:03 +0530

    commit message

---
+++ GrocceryApplication/src/main/java/pages/PageUtility.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class PageUtility {
+	public void selectDropdownWithValue(WebElement element, String value) {
+
+		Select object = new Select(element);
+		object.selectByValue(value);
+	}
+	public void selectDropdownByIndex(WebElement element,int index) {
+		Select object=new Select(element);
+		object.selectByIndex(index);
+	}
+	public void selectDropdownByVisibleText(WebElement element,String text) {
+		Select object=new Select(element);
+		object.selectByVisibleText(text);
+	}
+	public void clickButton(WebElement element) {
+		element.click();
+	}
+	public void clickCheckbox(WebElement element) {
+		element.click();
+	}
+	
+}
---
+++ GrocceryApplication/src/main/java/pages/WaitUtility.java
@@ -0,0 +1,44 @@
+package pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtility {
+	public static int EXPLICITWAIT = 5;
+
+	public void waitUntilElementToBeClickable(WebDriver driver, WebElement element) {
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
+
+	public void waitUntilElementVisible(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public void waitUntilElementPresent(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public void waitForElementInvisible(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+		wait.until(ExpectedConditions.invisibilityOf(element));
+	}
+
+	public void waitForAlertPresent(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+		wait.until(ExpectedConditions.alertIsPresent());
+	}
+
+	public void waitForTitleContains(WebDriver driver, String titleText) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICITWAIT));
+		wait.until(ExpectedConditions.titleContains(titleText));
+	}
+
+}
